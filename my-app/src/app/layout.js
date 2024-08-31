30fbb9e39ea519c58c74c2db7c31b2939caa3f3e
octaviopenaa <openaiannello@pioix.edu.ar>
2024-08-31 20:24:08 -0300

    posible commit a main

--- my-app/src/app/layout.js
+++ my-app/src/app/layout.js
@@ -3,34 +3,51 @@ import Titulo from "./components/title";
 import Input from "./components/inputs";
 import Button from "./components/button";
 import Chatbutton from "./components/chatButton";
+import Image from "next/image";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={styles.body}>
         <main className={styles.main}>
           <seccion className={styles.seccion_emotes}>
+            <Image
+              src="/images/iconoChat.png"
+              width={50}
+              height={50}
+              alt="iconochat"
+            />
+          </seccion>
 
-          </seccion>
           <seccion className={styles.seccion_chats}>
-            <header className={styles.header}>
-              <Titulo text="Chats"/>
-            </header>
-            <div>
-              <Input placeholder="Buscar"/>
-            </div>
-            <div className={styles.div_botones}>
-              <Button text="Todos"/>
-              <Button text="No leídos"/>
-              <Button text="Grupos"/>
-            </div>
-            <div className={styles.div_chatbuttons}>
-              <Chatbutton url="/photos/fotoDePerfilPredem.jpg" contacto="BautiPuto" ultimoMensaje="devolveme la plata gil" />
-              <Chatbutton url="/photos/fotoDePerfilPredem.jpg" contacto="BautiPuto" ultimoMensaje="devolveme la plata gil" />
-              <Chatbutton url="/photos/fotoDePerfilPredem.jpg" contacto="BautiPuto" ultimoMensaje="devolveme la plata gil" />
-              <Chatbutton url="/photos/fotoDePerfilPredem.jpg" contacto="BautiPuto" ultimoMensaje="devolveme la plata gil" />
-              <Chatbutton url="/photos/fotoDePerfilPredem.jpg" contacto="BautiPuto" ultimoMensaje="devolveme la plata gil" />
-            </div>
+            <seccion>
+              <header className={styles.header}>
+                <Titulo text="Chats"/>
+              </header>
+              <div>
+                <Input placeholder="Buscar" />
+              </div>
+              <div className={styles.div_botones}>
+                <Button text="Todos" />
+                <Button text="No leídos" />
+                <Button text="Grupos" />
+              </div>
+            </seccion>
+            <seccion className={styles.contactos}>
+              <div className={styles.div_chatbuttons}>
+                <Chatbutton srcImg="/images/fotoDePerfilPredem.jpg" alt="fotoPerfil" contacto="BautiPuto" ultimoMensaje="devolveme la plata gil" />
+                <Chatbutton srcImg="/images/fotoDePerfilPredem.jpg" alt="fotoPerfil" contacto="BautiPuto" ultimoMensaje="devolveme la plata gil" />
+                <Chatbutton srcImg="/images/fotoDePerfilPredem.jpg" alt="fotoPerfil" contacto="BautiPuto" ultimoMensaje="devolveme la plata gil" />
+                <Chatbutton srcImg="/images/fotoDePerfilPredem.jpg" alt="fotoPerfil" contacto="BautiPuto" ultimoMensaje="devolveme la plata gil" />
+                <Chatbutton srcImg="/images/fotoDePerfilPredem.jpg" alt="fotoPerfil" contacto="boca" ultimoMensaje="devolveme la plata gil" />
+                <Chatbutton srcImg="/images/fotoDePerfilPredem.jpg" alt="fotoPerfil" contacto="BautiPuto" ultimoMensaje="devolveme la plata gil" />
+                <Chatbutton srcImg="/images/fotoDePerfilPredem.jpg" alt="fotoPerfil" contacto="BautiPuto" ultimoMensaje="devolveme la plata gil" />
+                <Chatbutton srcImg="/images/fotoDePerfilPredem.jpg" alt="fotoPerfil" contacto="nashe" ultimoMensaje="devolveme la plata gil" />
+                <Chatbutton srcImg="/images/fotoDePerfilPredem.jpg" alt="fotoPerfil" contacto="BautiPuto" ultimoMensaje="devolveme la plata gil" />
+                <Chatbutton srcImg="/images/fotoDePerfilPredem.jpg" alt="fotoPerfil" contacto="insta" ultimoMensaje="devolveme la plata gil" />
+              </div>
+            </seccion>
           </seccion>
+
           <seccion>{children}</seccion>
         </main>
 
