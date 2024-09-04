811669537f44ad33e68b500edf4ec1a1d31b893e
octaviopenaa <openaiannello@pioix.edu.ar>
2024-09-04 08:47:52 -0300

    emoticonos y socket

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -1,15 +1,47 @@
+"use client"
 import styles from "./page.module.css"
 import Input from "./components/inputs";
+import Image from "next/image";
+import { useSocket } from "./hooks/useSocket";
+import { useEffect } from "react";
+import Button from "./components/button";
 
-export default function home(){
-    return(
+export default function home() {
+    const { socket, isConnected } = useSocket();
+
+    useEffect(() => {
+        // Evitar que genere errores si no esta el socket
+        if (!socket) return;
+
+        socket.on('pingAll', (data) => {
+            console.log("me llego mensaje", data);
+        })
+
+    }, [socket,isConnected]);
+
+
+    function handleClick() {
+        socket.emit('pingAll', {message: "facu pito chico"})
+    }
+
+    return (
         <div className={styles.div_child}>
             <div className={styles.barraContactos}>
-
+            <Button onClick={handleClick} text="Enviar pingALL"/>
             </div>
             <div className={styles.barraDeEscribir}>
-                <div className={styles.escribirDiv}>
-                    <Input className={styles.inputEscribir} placeholder="Escribir" />
+                <div>
+                    <div className={styles.escribirDiv}>
+                        <Input className={styles.inputEscribir} placeholder="Escribir" />
+                    </div>
+                    <div>
+                        <Image className={styles.iconoMicro}
+                            src="/images/icono_Micro.png"
+                            width={35}
+                            height={35}
+                            alt="iconoMicrofono"
+                            />
+                    </div>
                 </div>
             </div>
         </div>
